4fac18c4bbe9424a3772cd40521f99c157f82ca1
pawelc91 <pawelxcieslak@gmail.com>
2019-11-19 19:01:10 +0100

    19.11v3

---
+++ src/oop/Main.java
@@ -0,0 +1,37 @@
+package oop;
+
+import javax.xml.namespace.QName;
+
+// klasa główna - uruchumieniowa
+public class Main {
+    public static void main(String[] args) {
+        // utworzenie obiektu
+        User userOne = new User();
+        // odwołanie do pola klasowego
+        userOne.name = "Michał";
+        userOne.lastName = "Kruczkowski";
+        userOne.activated = true;
+        userOne.salary_net = 10100;
+        userOne.gender = 'M';
+        // wywołanie metody
+        userOne.printUser();
+
+        User userTwo = new User("Adam","Kowalski",'M',false,4000);
+        userOne.printUser();
+
+        User userThree = new User("Paweł", "Cieślak", 'M',false,3000);
+        userThree.printUser();
+
+        userOne.salary_net = 95000;
+        System.out.println("===============================");
+        userOne.printUser();
+        userTwo.printUser();
+        userThree.printUser();
+        System.out.println("===============================");
+
+        double salaryGross = userOne.calculateSalaryGross();
+        System.out.println("salaryGross = " + salaryGross);
+        System.out.println("Calculated calary gross: " + userTwo.calculateSalaryGross());
+    }
+
+}
