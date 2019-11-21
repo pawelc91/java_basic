03c962bc7b4e57173068bb10979d0d013a3d94e3
pawelc91 <pawelxcieslak@gmail.com>
2019-11-21 20:29:10 +0100

    21.11v3

---
+++ src/main/java/datetimeVariables/ExerciseDateTime.java
@@ -0,0 +1,23 @@
+package datetimeVariables;
+
+import java.time.LocalDate;
+
+public class ExerciseDateTime {
+    public static void main(String[] args) {
+        // znajdź dzień tygodnia w którym się urodziałeś/urodziałaś
+        LocalDate birthDate = LocalDate.of(1963,4,07);
+        int myDayOfWeek = birthDate.getDayOfWeek().getValue();
+        System.out.println(myDayOfWeek);
+        // znajdź ile razy do dziś Twoje urodziny przypadały
+        // w ten sam dzień tygodnia w którym się urodziłeś/urodziłaś
+        LocalDate actual = LocalDate.now();
+        int counter = 0;
+        for (birthDate.getYear(); birthDate.getYear() <= actual.getYear(); birthDate = birthDate.plusYears(1)) {
+            if(myDayOfWeek == birthDate.getDayOfWeek().getValue()){
+                System.out.println(birthDate);
+                counter++;
+            }
+        }
+        System.out.println(counter - 1);
+    }
+}
